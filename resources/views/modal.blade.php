992801bef56e5d51091570b81cce095a754968f6
henrysetiana <38009124+henrysetiana@users.noreply.github.com>
2020-09-15 18:57:25 +0700

    update modal validation

--- resources/views/modal.blade.php
+++ resources/views/modal.blade.php
@@ -8,6 +8,12 @@
     $error_fields = explode("|",strtoupper($modal['stgreject']['error_fields']));
     // var_dump($error_fields);
   }
+  $error_descs = [];
+  if(isset($modal['stgreject']['error_desc']))
+  {
+    $error_descs = explode("|",strtoupper($modal['stgreject']['error_desc']));
+    // var_dump($error_fields);
+  }
 ?>
 <?php foreach ($modal['column'] as $key => $value): ?>
 <?php if ( isset($modal['stgreject'][$key]) ): ?>
@@ -21,6 +27,12 @@
       <div class="col-sm-8">
           <input type="text" class="form-control {{$input_class}}" id="{{$key}}" name="{{$key}}" <?php if(isset($value["editable"]) && !$value["editable"] || $input_class!="has_error") echo "disabled";?>
                  placeholder="Enter String" value="{{ $modal['stgreject'][$key] }}">
+          <?php foreach($error_descs as $error_desc):?>
+            <?php //echo explode(" ",$error_desc)[0];?>
+            <?php if(strtoupper(explode(" ",$error_desc)[0])==strtoupper($key)):?>
+              <span style="color:red; font-size:70%;">{!!$error_desc!!}</span><br/>
+            <?php endif;?>
+          <?php endforeach;?>
       </div>
   </div>
 <?php endif; ?>
